63165795e4843f0005e4544376a8d2b82a25a254
Braden Katzman <bradenkatzman@gmail.com>
2016-06-28 00:46:51 -0400

    * implemented first run Spectral Clustering

--- HW3/Problem 3/load_prep.py
+++ HW3/Problem 3/load_prep.py
@@ -9,40 +9,30 @@
 # MAIN SOURCE USED:  http://scikit-learn.org/stable/auto_examples/cluster/plot_color_quantization.html
 # source: http://stackoverflow.com/questions/25528654/numpy-array-to-png-file-and-back
 
-from skimage.io import imread, imsave
+from scipy import misc
 import numpy as np
+import warnings
 
 def loadImage(filename):
 	print "\nloading image '{filename}'".format(filename=filename)
 
-	trees = imread(filename)
+	warnings.filterwarnings("ignore") # ignore uint8 to float64 warning
+	# load file as np array
+	trees = misc.imread(filename)
 
 	return trees
 
-
-# From main source:
-# Convert to floats instead of the default 8 bits integer coding. Dividing by
-# 255 is important so that plt.imshow behaves works well on float data (need to
-# be in the range [0-1]
-def convertImgToFloats(img):
-	print "\nconverting image data to floats in range [0,1]"
-
-	img = np.array(img, dtype=np.float64) / 255
-
-	return img
-
 # transform an image into a 2D numpy array
-def transformTo2DArray(img):
-	print "\ntransforming image into 2D numpy array"
+def transformToArray(img):
+	print "\ntransforming image into numpy array"
 
 	width, height, dimensions = orig = tuple(img.shape)
 
 	# print str(width) + ", " + str(height) + ", " + str(dimensions)
 
-	# error check and make sure the original dimensions of the image is 3
-	assert dimensions == 3
-
 	# convert to array
 	img_as_array = np.reshape(img, (width * height, dimensions))
+	print type(img_as_array)
+	print dimensions
 
 	return img_as_array, width, height, dimensions
