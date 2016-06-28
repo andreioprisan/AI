4dd3abd204a09d8046bffb121bf4452166694f72
Braden Katzman <bradenkatzman@gmail.com>
2016-06-28 00:50:22 -0400

    added example code

--- HW3/Problem 3/SpectralClustering.py
+++ HW3/Problem 3/SpectralClustering.py
@@ -51,44 +51,55 @@ def run(img_array):
 	plt.show()
 
 def run2():
-	lena = sp.misc.lena()
-	# Downsample the image by a factor of 4
-	lena = lena[::2, ::2] + lena[1::2, ::2] + lena[::2, 1::2] + lena[1::2, 1::2]
-	lena = lena[::2, ::2] + lena[1::2, ::2] + lena[::2, 1::2] + lena[1::2, 1::2]
+	if sp_version < (0, 12):
+	    raise SkipTest("Skipping because SciPy version earlier than 0.12.0 and "
+	                   "thus does not include the scipy.misc.face() image.")
+
+
+	# load the raccoon face as a numpy array
+	try:
+	    face = sp.face(gray=True)
+	except AttributeError:
+	    # Newer versions of scipy have face in misc
+	    from scipy import misc
+	    face = misc.face(gray=True)
+
+	# Resize it to 10% of the original size to speed up the processing
+	face = sp.misc.imresize(face, 0.10) / 255.
 
 	# Convert the image into a graph with the value of the gradient on the
 	# edges.
-	graph = image.img_to_graph(lena)
+	graph = image.img_to_graph(face)
 
 	# Take a decreasing function of the gradient: an exponential
 	# The smaller beta is, the more independent the segmentation is of the
 	# actual image. For beta=1, the segmentation is close to a voronoi
 	beta = 5
 	eps = 1e-6
-	graph.data = np.exp(-beta * graph.data / lena.std()) + eps
+	graph.data = np.exp(-beta * graph.data / graph.data.std()) + eps
 
 	# Apply spectral clustering (this step goes much faster if you have pyamg
 	# installed)
-	N_REGIONS = 11
+	N_REGIONS = 4
 
-	###############################################################################
+	#############################################################################
 	# Visualize the resulting regions
 
 	for assign_labels in ('kmeans', 'discretize'):
 	    t0 = time.time()
 	    labels = spectral_clustering(graph, n_clusters=N_REGIONS,
-	                                 assign_labels=assign_labels,
-	                                 random_state=1)
+	                                 assign_labels=assign_labels, random_state=1)
 	    t1 = time.time()
-	    labels = labels.reshape(lena.shape)
+	    labels = labels.reshape(face.shape)
 
 	    plt.figure(figsize=(5, 5))
-	    plt.imshow(lena,   cmap=plt.cm.gray)
+	    plt.imshow(face, cmap=plt.cm.gray)
 	    for l in range(N_REGIONS):
 	        plt.contour(labels == l, contours=1,
-	                    colors=[plt.cm.spectral(l / float(N_REGIONS)), ])
+	                    colors=[plt.cm.spectral(l / float(N_REGIONS))])
 	    plt.xticks(())
 	    plt.yticks(())
-	    plt.title('Spectral clustering: %s, %.2fs' % (assign_labels, (t1 - t0)))
-
+	    title = 'Spectral clustering: %s, %.2fs' % (assign_labels, (t1 - t0))
+	    print(title)
+	    plt.title(title)
 	plt.show()
